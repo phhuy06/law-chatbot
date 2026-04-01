8e28016f0880a2c65ca6dda55a9eea9fc73cde46
CanhHuy <Canhhuy2004@gmail.com>
2026-04-01 16:57:44 +0700

    add document test

--- spark/testweed1.py
+++ spark/testweed1.py
@@ -4,14 +4,17 @@ import sys
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, col
 from spark.utils.udfs import clean_text, chunk_text
 
 spark = SparkSession.builder.appName("TestUDFs").getOrCreate()
-df = spark.read.option("multiline", "true").json("test_data/sample_docs.json")
+json_path = os.path.join(project_root, "test_data", "sample_docs.json")
+df = spark.read.option("multiline", "true").json(json_path)
 df.printSchema()
-
 print(df.columns)
 
 df_clean = df.withColumn("clean_text", clean_text(col("content_html")))
