b5a08e55739a54f8c9415ab69b7544bd3484c477
thebluebsss <167521513+thebluebsss@users.noreply.github.com>
2026-04-08 23:43:30 +0700

    crawl

--- crawler/playwright_scrape.py
+++ crawler/playwright_scrape.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 from typing import Dict, List
 
 BASEDIR = os.path.dirname(__file__)
-OUTPUT = os.path.join(BASEDIR, "output", "quyen-dan-su.csv")
-START_URL = "https://thuvienphapluat.vn/hoi-dap-phap-luat/quyen-dan-su"
+OUTPUT = os.path.join(BASEDIR, "output", "tai-chinh-nha-nuoc.csv")
+START_URL = "https://thuvienphapluat.vn/hoi-dap-phap-luat/tai-chinh-nha-nuoc"
 START_PAGE = 1
 END_PAGE = 5
 
